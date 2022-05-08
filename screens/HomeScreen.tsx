aeacb5759fbdb133a86052b6d363f63a8924d727
MG2496 <michaelgamboamoya24@gmail.com>
2022-05-08 12:31:27 -0600

    style

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,13 +2,17 @@ import { View } from "react-native";
 import { Text } from "react-native";
 import { StyleSheet } from "react-native";
 import React from "react";
+import {Platform, Dimensions} from 'react-native'
 
 export default function HomeScreen(){
     return (
         <View style={styles.container}>
-            <Text>
+            <Text style={styles.text}>
                 Home!🥸
             </Text>
+            {/* <Text style = {styles.textShow}> El ancho de la pantalla es: {Dimensions.get ('window').width}</Text>
+            <Text style = {styles.textShow}> La altura de la pantalla es: {Dimensions.get ('window').height}</Text>
+            <Text style = {styles.textShow}> La resolución de la pantalla actual: {Dimensions.get ('window').scale}</Text> */}
         </View>
     );
 }
@@ -16,9 +20,30 @@ export default function HomeScreen(){
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        color: 'brown',
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        //alignItems: 'center',
+        //justifyContent: 'center',
+        
     },
+
+    text:{
+        color: "brown",
+        fontSize: 30,
+        justifyContent: 'center',
+        backgroundColor: '#d8d2cc',
+    
+    },
+// dimensiones
+    // textShow: {
+    //     marginLeft:15,
+    //     marginRight:15,
+    //     marginTop:40,
+    //     textAlign:'center',
+    //     color:'#187ae6',
+    //     fontSize:20,
+    //     fontWeight:'bold',
+    // },
+
+
+    
 });
